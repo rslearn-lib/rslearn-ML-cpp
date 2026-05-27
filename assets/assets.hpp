ad92ef6f3ae683be1ec12c418ffc99f2d1f136fc
Rustam Singh <rustam@localhost.localdomain>
2026-05-27 12:43:24 +0530

    API Design - Tensor Schema

---
+++ assets/assets.hpp
@@ -0,0 +1,19 @@
+/*
+* Copyright (c) <YEAR>, rslearn-lib
+* All rights reserved.
+* (Full BSD-3-Clause license text here)
+*/
+
+/*
+This File Contains Important assets for rslearn-ml C++ version
+It Includes things like
+dot multiplication
+shape validation
+dimensional maximization
+tensors
+*/
+
+#pragma once
+
+#include <iostream>
+#include "numpy/tensor.hpp"
