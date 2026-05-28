6587d09eb6fc32e40dea65c3ee7cc02e2d205ea3
Rustam <rustambhadouriya7@gmail.com>
2026-05-28 16:39:24 +0530

    feat: Tensor 1D & 2D

--- assets/numpy/tensor.hpp
+++ assets/numpy/tensor.hpp
@@ -7,14 +7,198 @@
 // File to Create Tensor in C++ 
 // like np.array named as tensor
 
+
+// HOW TO USE IT?
+
+/*
+CONTRIBUTER & DEVELOPER GUID   
+RECOMMANDED EXPERIENCE IN C++ : atleast 1 years  
+NOTE: THIS GUID IS NOT FOR BEGINNERS
+
+*/
+
+/*
+It's Simple to use it
+
+Tensor 1D
+----------
+Core Data Structure Used for `y` and dot production's output & `weights`
+
+    importation
+    -----------
+    `#include "assets/numpy/tensor.hpp"`  
+
+    Initlization
+    ------------
+
+    `tensor1D<double> tensor({1.2, 1.0, 3.5, 10.0});`  
+
+    Things to Remember
+    ------------------
+    * Use Dtype double for better gradient  
+    * Input array not be Empty or better way to use externel array like  
+        `double arr[5] = {1.2, 1.0, 3.5, 10.0, 15.1}`  
+        `tensor1D<double> tensor(arr)`  
+    * array type and tensor type must be match double or int, double recommanded
+
+    Features/Methods/attribute  
+    --------------------------
+    * shape (attribute) -> returns vector {rows, col}, usage `tensor.shape`  
+    * at (Method) Input(index) -> returns value at index, usage tensor.at(2)  
+    * size (Method) -> returns total length of tensor same as shape[0], usage `tensor.size()`  
+
+Tensor 2D
+---------
+Core Data Structure for Data Processing and mainly for `X`  
+    Importation
+    -----------
+    `#include "assets/numpy/tensor.hpp"`  
+
+    Initlization
+    ------------
+    
+    `tensor2D<double> tensor({{1.0, 2.4, 3.3}, {4.2, 5.5, 6.1}})`  
+
+    Things To Remmember
+    -------------------
+    * No Empty Array for stability and clear control & Better Way to Write  
+        `double arr[2][3] = {{1.0, 2.4, 3.3}, {4.2, 5.5, 6.1}}`  
+        `tensor2D<double> tensor(arr)`  
+    * array type and tensor type must be match double or int, double recommanded
+
+    Features/Methods/attribute  
+    --------------------------
+    * shape (attribute) -> returns vector {rows, col}, usage `tensor.shape`  
+    * at (Method) Input(row, col) -> returns value at index= (row*total_col) + col, usage tensor.at(1, 2)  
+    * size (Method) -> returns total length of tensor same as shape[0], usage `tensor.size()`  
+
+
+
+*/
+
+#ifndef TENSOR_HPP
+#define TENSOR_HPP
+
 #pragma once
 
 #include <iostream>
 #include <vector>
+#include <initializer_list>
 
-class tensor {
+
+// Tensor 1D class for rslearn-ml for y and output data and normal processing
+// this is core Data Sctructure in this project
+// NOTE for contibuters: USE IT
+// and checkout Tensor 2D
+// rslearn-ML-cpp/assets/numpy/tensor.cpp - tensor1D
+template <typename T>
+class tensor1D {
+    private:
+
+        std::vector<T> data;
+        
     public:
-        tensor(){
-            std::cout << "Hello Tensor File Called";
+        int ndim = 1;
+        std::vector<int> shape = {0, 0};
+
+        tensor1D(T value){
+            data.push_back(value);
+            shape[0] = 1;
+        }
+
+       
+        tensor1D(std::initializer_list<T> list): data(list) {
+            if (data.empty()){
+                throw std::invalid_argument("Tensor can't be empty");
+            }
+            shape[0] = data.size();
+        } // Direct Case 
+
+        template <size_t N>
+        tensor1D(const T (&rawArray)[N]) : data(rawArray, rawArray + N) {
+            shape[0] = data.size();
+        }
+
+        void printData() const{
+            for (const auto& item : data){
+                std::cout << item << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        T at(size_t index) const{
+            if (index >= shape[0]){
+                throw std::out_of_range("Index Out Of Range");
+            }
+
+            return data.at(index);
+        }
+
+        size_t size() const{
+            return shape[0];
         }
 };
+
+// Tensor 2D class for rslearn-ml for y and output data and normal processing
+// this is also core Data Sctructure in this project
+// NOTE for contibuters: USE IT EVERYWHERE 'CAUSE WHOLE CLASSES USES IT
+// and checkout Tensor 1D 
+// rslearn-ML-cpp/assets/numpy/tensor.cpp -  tensor2D
+template <typename T>
+class tensor2D{
+    private:
+        std::vector<std::vector<T>> data;
+    
+    public:
+        std::vector<int> shape;
+        int ndim = 2; 
+
+        // Case like tensor2D<int> data({{1, 2}, {3, 4}});
+        tensor2D(std::initializer_list<std::vector<T>> list): data(list){
+            if (data.empty()){
+                throw std::invalid_argument("Tensor can't be empty");
+            }
+            int rows = data.size();
+            int col = data[0].size();
+            shape = {rows, col};
+        }
+
+        template <size_t ROWS, size_t COLS>
+        tensor2D(const T (&rawArray)[ROWS][COLS]){
+            shape = {
+                static_cast<int>(ROWS),
+                static_cast<int>(COLS)
+            };
+            
+            for(size_t i=0; i < ROWS; i++){
+                data.emplace_back(
+                    rawArray[i],
+                    rawArray[i] + COLS
+                );
+            }
+
+        }
+
+        T at(size_t row, size_t col) const{
+            if(row >= shape[0] || col >= shape[1]){
+                throw std::out_of_range("Invalid row, col");
+            }
+            return data[row][col];
+        }
+
+        size_t size() const{
+            return shape[0];
+        }
+
+        void printData() const{
+            for(size_t row = 0; row < shape[0]; ++row){
+                for(size_t col = 0; col < shape[1]; ++col){
+                    std::cout << data[row][col] << " ";
+                }
+                std::cout << "\n";
+            }
+        }
+
+};
+
+#endif
---
+++ learn/learn_tensors.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "../assets/assets.hpp" // Importing for directry
+#include <vector>
+
+// FOR USER IMPORT "assets/assets.hpp"
+
+
+// COMPILE & RUN GUIDE
+/*
+COMPILE
+tested in g++ 16.1.1 and latest C++
+
+NOTE: add .out if you use MacOS and .exe if you use Windows in the end on l_tens.<ext>
+
+// if in same directry where learn_tensors.cpp is
+g++ learn_tensors.cpp -o l_tens
+
+// outside of learn/ folder
+g++ learn/learn_tensors.cpp -o l_tens
+
+RUN
+
+LINUX
+./l_tens
+
+WINDOWS
+./l_tens.exe
+
+MACOS
+./l_tens.out
+
+*/
+
+int main(){
+
+    // 1D testing
+
+    int arry[] = {1, 2, 3};
+
+    // Case one
+    tensor1D<int> tens(arry); // externel array
+    tens.printData();
+
+    // Case Two
+
+    tensor1D<int> tens2({10, 20, 30}); // init_list array
+    tens2.printData();
+
+    // Case Three
+    tensor1D<int> tens3(67); // single value (only in 1D)
+    tens3.printData();
+
+    // std::cout << tens.shape() << std::endl;
+    std::vector<int> shape = tens.shape;
+    std::cout << shape[0] << std::endl;
+    std::cout << tens.size() << std::endl;
+    std::cout << tens.at(1) << std::endl;
+
+
+    // 2D Testing
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    tensor2D<int> tens2d({{1, 2, 3}, {4, 5, 6}}); // init_list array case
+    std::cout << tens2d.size() << std::endl;
+    tens2d.printData();
+    for(auto& item : tens2d.shape){
+        std::cout << item << " ";
+    }
+    std::cout << "\n";
+
+
+    std::cout << tens2d.at(1, 2) << std::endl; 
+
+    tensor2D<int> tens2d2(arr); // externel array
+    std::cout << tens2d2.size() << std::endl;
+    tens2d2.printData();
+    for(auto& item : tens2d2.shape){
+        std::cout << item << " ";
+    }
+    std::cout << "\n";
+
+
+    std::cout << tens2d2.at(1, 2) << std::endl;
+    return 0;
+}
