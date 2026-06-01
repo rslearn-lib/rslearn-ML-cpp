bab2cee89293e27a3522db9e8bfedb3dbc24fc84
Rustam <rustambhadouriya7@gmail.com>
2026-06-01 12:58:46 +0530

    Feat: sum feature in tensors 1D, 2D both

---
+++ assets/numpy/maths.hpp
@@ -0,0 +1,12 @@
+/*
+* Copyright (c) 2026, rslearn-lib, rslearn-ML
+* All rights reserved.
+* (Full BSD-3-Clause license)
+*/
+
+// File to Create Some NumPy Math Feature in C++ 
+// like sum, mean, average, sub etc
+
+/*
+
+*/
